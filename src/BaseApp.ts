939c7956ae015a764be046b681b44e5f3d38a968
David De Anda <omarandstuff@gmail.com>
2022-07-20 23:24:21 -0500

    Load config and select to pass to app

--- src/BaseApp.ts
+++ src/BaseApp.ts
@@ -1,7 +1,19 @@
-export default class BaseApp {
+import Logger from '@universal-packages/logger'
+
+export default class BaseApp<O = any, A = any> {
   public static readonly appName: string
   public static readonly appShortName: string
   public static readonly description: string
+
+  protected readonly options: O
+  protected readonly args: A
+  protected readonly logger: Logger
+
+  public constructor(options: O, args: A, logger: Logger) {
+    this.options = options
+    this.args = args
+    this.logger = logger
+  }
 
   public async load(): Promise<void> {
     throw 'Implement me: Apps should implement the load amethod'
--- src/core-app.ts
+++ src/core-app.ts
@@ -1,3 +1,4 @@
+import { loadConfig } from '@universal-packages/config-loader'
 import Logger, { TerminalTransport } from '@universal-packages/logger'
 import { loadModules, ModuleRegistry } from '@universal-packages/module-loader'
 import { loadPluginConfig } from '@universal-packages/plugin-config-loader'
@@ -7,13 +8,16 @@ import BaseApp from './BaseApp'
 import { coreConfigSchema } from './core-app.schema'
 import { CoreAppConfig } from './core-app.types'
 
-const globals = {
+const coreCapsule = {
   App: null,
+  allConfig: null,
+  appConfig: null,
   appInstance: null,
+  appParamCaseName: null,
+  appPascalCaseName: null,
   args: null,
   coreAppConfig: null,
-  logger: new Logger(),
-  errored: false
+  logger: new Logger()
 }
 
 export async function startApp(name: string, args: Record<string, any>): Promise<void> {
@@ -23,6 +27,9 @@ export async function startApp(name: string, args: Record<string, any>): Promise
   if (!proceed) return
 
   proceed = await loadCoreAppConfig()
+  if (!proceed) return
+
+  proceed = await loadAppConfig()
   if (!proceed) return
 
   proceed = await loadApp(name, args)
@@ -35,7 +42,7 @@ export async function execTask(name: string, directive: string, directiveOptions
 
 function initLogger(): boolean {
   try {
-    const termianlTransport = globals.logger.getTransport('terminal') as TerminalTransport
+    const termianlTransport = coreCapsule.logger.getTransport('terminal') as TerminalTransport
     termianlTransport.setCategoryColor('CORE', 'BLACK')
   } catch (error) {
     console.log(error)
@@ -61,71 +68,96 @@ async function loadCoreAppConfig(): Promise<boolean> {
   if (errors.length > 0) {
     const errorMessages = errors.map((error: any): string => `${error.path} - ${error.message}`)
 
-    globals.logger.publish('ERROR', 'Core configuration validation error', null, 'CORE', {
+    coreCapsule.logger.publish('ERROR', 'Core configuration validation error', null, 'CORE', {
       metadata: { errors: errorMessages },
       measurement: measurer.finish().toString()
     })
 
-    await globals.logger.await()
+    await coreCapsule.logger.await()
     return false
   } else {
-    if (finalCoreAppConfig.logger?.level) globals.logger.level = finalCoreAppConfig.logger.level
-    if (finalCoreAppConfig.logger?.silence === true) globals.logger.silence = true
+    if (finalCoreAppConfig.logger?.level) coreCapsule.logger.level = finalCoreAppConfig.logger.level
+    if (finalCoreAppConfig.logger?.silence === true) coreCapsule.logger.silence = true
 
-    globals.logger.publish('DEBUG', 'Core Configuration loaded', null, 'CORE', { metadata: finalCoreAppConfig, measurement: measurer.finish().toString() })
+    coreCapsule.logger.publish('DEBUG', 'Core Configuration loaded', null, 'CORE', { metadata: finalCoreAppConfig, measurement: measurer.finish().toString() })
 
-    globals.coreAppConfig = finalCoreAppConfig
+    coreCapsule.coreAppConfig = finalCoreAppConfig
 
     return true
   }
 }
 
-async function loadApp(name: string, args: Record<string, any>): Promise<boolean> {
+async function loadAppConfig(): Promise<boolean> {
   const measurer = startMeasurement()
 
-  const pascalCaseName = pascalCase(name)
-  const paramCaseName = paramCase(name)
-  const apps = await loadModules(globals.coreAppConfig.appsDirectory, { conventionPrefix: 'app' })
+  try {
+    const loadedAppConfig = await loadConfig(coreCapsule.coreAppConfig.configDirectory, { selectEnvironment: true })
+
+    coreCapsule.logger.publish('DEBUG', 'Core Configuration loaded', null, 'CORE', { metadata: loadedAppConfig, measurement: measurer.finish().toString() })
+
+    coreCapsule.allConfig = loadedAppConfig
+  } catch (error) {
+    coreCapsule.logger.publish('ERROR', 'There was an error loading the config (The one from teh config dir)', null, 'CORE', {
+      error,
+      measurement: measurer.finish().toString()
+    })
+
+    return false
+  }
+
+  return true
+}
+
+async function loadApp(name: string, args: Record<string, any>): Promise<boolean> {
+  const measurer = startMeasurement()
+  const appPascalCaseName = pascalCase(name)
+  const appParamCaseName = paramCase(name)
+
+  const apps = await loadModules(coreCapsule.coreAppConfig.appsDirectory, { conventionPrefix: 'app' })
   const appModuleRegistry = apps.find((module: ModuleRegistry): boolean => {
-    const fileMatches = !!module.location.match(new RegExp(`(${pascalCaseName}|${paramCaseName}).app\..*$`))
+    const fileMatches = !!module.location.match(new RegExp(`(${appPascalCaseName}|${appParamCaseName}).app\..*$`))
 
     return module.exports ? module.exports.appShortName === name || module.exports.name === name || fileMatches : fileMatches
   })
 
   if (!appModuleRegistry) {
-    globals.logger.publish({
+    coreCapsule.logger.publish({
       level: 'ERROR',
       title: `No app named ${name} found`,
       category: 'CORE'
     })
-    await globals.logger.await()
+    await coreCapsule.logger.await()
     return false
   } else if (appModuleRegistry.error) {
-    globals.logger.publish({
+    coreCapsule.logger.publish({
       level: 'ERROR',
       title: 'There was an error loading the App',
       message: `App "${name}" was loadaded with errors`,
       category: 'CORE',
       error: appModuleRegistry.error
     })
-    await globals.logger.await()
+    await coreCapsule.logger.await()
     return false
   } else if (!(appModuleRegistry.exports.prototype instanceof BaseApp)) {
-    globals.logger.publish({
+    coreCapsule.logger.publish({
       level: 'ERROR',
       title: 'Uable to load App',
       message: `App "${name}" seems to not be a class inheriting from BaseApp`,
       category: 'CORE'
     })
-    await globals.logger.await()
+    await coreCapsule.logger.await()
     return false
   }
 
+  coreCapsule.appPascalCaseName = appPascalCaseName
+  coreCapsule.appParamCaseName = appParamCaseName
+  coreCapsule.appConfig = coreCapsule.allConfig[appParamCaseName] || coreCapsule.allConfig[appPascalCaseName] || {}
+
   try {
-    globals.App = appModuleRegistry.exports
-    globals.appInstance = new globals.App(globals, args)
+    coreCapsule.App = appModuleRegistry.exports
+    coreCapsule.appInstance = new coreCapsule.App(coreCapsule.appConfig, args, coreCapsule.logger)
   } catch (error) {
-    globals.logger.publish({
+    coreCapsule.logger.publish({
       level: 'ERROR',
       title: 'There was an error instantiating the App',
       category: 'CORE',
@@ -135,9 +167,9 @@ async function loadApp(name: string, args: Record<string, any>): Promise<boolean
   }
 
   try {
-    await globals.appInstance.load()
+    await coreCapsule.appInstance.load()
   } catch (error) {
-    globals.logger.publish({
+    coreCapsule.logger.publish({
       level: 'ERROR',
       title: 'There was an error running the App load routine',
       category: 'CORE',
@@ -146,7 +178,7 @@ async function loadApp(name: string, args: Record<string, any>): Promise<boolean
     return false
   }
 
-  globals.logger.publish('INFO', `App ${name} loaded`, null, 'CORE', { measurement: measurer.finish().toString() })
+  coreCapsule.logger.publish('INFO', `App ${name} loaded`, null, 'CORE', { measurement: measurer.finish().toString() })
 
   return true
 }
