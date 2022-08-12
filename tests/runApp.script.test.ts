a06c7730a44c2d67b633c27371d83351cfa3fea8
David De Anda <omarandstuff@gmail.com>
2022-08-11 20:36:54 -0500

    runApp script specs

---
+++ tests/runApp.script.test.ts
@@ -0,0 +1,19 @@
+import { runApp } from '../src/runApp'
+
+jest.mock('../src/runApp')
+
+describe('runApp.script', (): void => {
+  it('do all the preaprations funds an app and runs it (sets core)', async (): Promise<void> => {
+    const appName = 'super-app'
+    const args = { fast: true }
+    const demon = true
+
+    process.env['CORE_APP_NAME'] = appName
+    process.env['CORE_APP_ARGS'] = JSON.stringify(args)
+    process.env['CORE_DEMON'] = 'true'
+
+    require('../src/runApp.script')
+
+    expect(runApp).toHaveBeenCalledWith(appName, args, demon)
+  })
+})
