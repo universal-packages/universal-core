537c67a6cd57d06f50030099b29268328aac3d52
David De Anda <omarandstuff@gmail.com>
2022-08-16 20:37:17 -0500

    Use owen ts config if not suplied

--- bin/ucore.js
+++ bin/ucore.js
@@ -1,26 +1,23 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+const path = require('path')
+
 try {
   require('@babel/register')
 } catch (_) {
-  require('ts-node').register()
+  const spawnerHasTsConfig = fs.existsSync('./tsconfig.json')
+
+  require('ts-node').register({ cwd: spawnerHasTsConfig ? './' : path.resolve(__dirname, '..') })
 }
 
 try {
-  try {
-    require('../src/cli')
-  } catch (_) {
-    require('../cli')
-  }
+  require('../src/cli')
 } catch (error) {
   // There was an actual error in this TS library
   if (process[Symbol.for('ts-node.register.instance')] !== undefined) {
     console.log(error)
   } else {
-    try {
-      require('../src/cli')
-    } catch (_) {
-      require('../cli')
-    }
+    require('../cli')
   }
 }
