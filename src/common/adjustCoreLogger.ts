60bd5326d1cb470877cfbbf82492f9277f0add3a
David De Anda <omarandstuff@gmail.com>
2024-03-28 14:32:57 -0600

    Pass terminal presenter transport directly from core

--- src/common/adjustCoreLogger.ts
+++ src/common/adjustCoreLogger.ts
@@ -1,7 +1,9 @@
 import { Logger } from '@universal-packages/logger'
+import { TerminalPresenterTransport } from '@universal-packages/logger-terminal-presenter'
 
 export async function adjustCoreLogger(): Promise<void> {
   core.logger = new Logger({
+    includeTransportAdapters: { 'terminal-presenter': TerminalPresenterTransport },
     level: process.env.NODE_ENV === 'test' ? 'ERROR' : 'TRACE',
     silence: !!process.env.CORE_TESTING,
     transports: ['terminal-presenter', 'local-file'],
--- src/common/initCoreLogger.ts
+++ src/common/initCoreLogger.ts
@@ -1,8 +1,14 @@
 import { Logger } from '@universal-packages/logger'
+import { TerminalPresenterTransport } from '@universal-packages/logger-terminal-presenter'
 
 export async function initCoreLogger(): Promise<void> {
   if (!core.logger) {
-    core.logger = new Logger({ level: process.env.NODE_ENV === 'test' ? 'ERROR' : 'TRACE', silence: !!process.env.CORE_TESTING, transports: ['terminal-presenter', 'local-file'] })
+    core.logger = new Logger({
+      includeTransportAdapters: { 'terminal-presenter': TerminalPresenterTransport },
+      level: process.env.NODE_ENV === 'test' ? 'ERROR' : 'TRACE',
+      silence: !!process.env.CORE_TESTING,
+      transports: ['terminal-presenter', 'local-file']
+    })
 
     await core.logger.prepare()
   }
