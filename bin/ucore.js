99d01968be98c7db221cd8575ee6db21affd56ba
David De Anda <omarandstuff@gmail.com>
2024-11-30 01:02:17 -0600

    Use tsx and update base libs

--- bin/ucore.js
+++ bin/ucore.js
@@ -1,15 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --import tsx
 
 const fs = require('fs')
 const path = require('path')
-
-const isTsProject = fs.existsSync('./tsconfig.json')
-
-if (isTsProject) {
-  require('ts-node').register()
-} else {
-  require('@babel/register')
-}
 
 try {
   const compiledCli = fs.existsSync(path.resolve(__dirname, '..', 'cli.js'))
